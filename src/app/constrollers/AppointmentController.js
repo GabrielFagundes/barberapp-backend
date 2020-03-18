8e9ffcbdb6003187d18ff60be02292fc9b55e52b
GabrielFagundes <aluzgabriel@hotmail.com>
2020-03-18 15:46:40 -0300

    Add mongodb to notification use

--- src/app/constrollers/AppointmentController.js
+++ src/app/constrollers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notifcation from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -79,6 +81,16 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart
+    });
+
+    const user = await User.findByPk(req.userId);
+    const formatedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+      locale: pt
+    });
+
+    await Notifcation.create({
+      content: `Novo agendamento de ${user.name} para o ${formatedDate}`,
+      user: provider_id
     });
 
     return res.json(appointment);
